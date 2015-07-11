eed114f280f2644c679bd9ee06112b4a539829e1
Brendon Brewer <brendon.brewer@gmail.com>
2015-07-11 13:26:43 +1200

    Asking questions and computing final position

--- main.py
+++ main.py
@@ -9,9 +9,25 @@ pos = np.zeros(3)
 
 input_text = 'Enter response from -2 (strongly disagree) to +2 (strongly agree): '
 
-# Using a C-style loop without apology
+# Using a C-style loop over questions without apology
 for i in range(0, questions.shape[0]):
+
+	# Check the question satisfies a basic sanity check
+	norm = np.linalg.norm(questions.iloc[i, 1:])
+	if norm > 2.:
+		print('# WARNING: Very influential question.')
+	elif norm < 0.5:
+		print('# WARNING: Very uninfluential question.')
+
+	# Print the question
 	print('\nQuestion {k}:\n'.format(k=i+1))
 	print(questions.iloc[i, 0] + '\n')
-	response = input(input_text)
+
+	# Get the user's response
+	response = None	# Placeholder value
+	while response < -2. or response > 2.:
+		response = input(input_text)
+
+	# Increment the user's position
+	pos += response*questions.iloc[i, 1:].values
 
