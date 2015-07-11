027c85f090b071b88fe84d5c41a9eca85d92092e
Brendon Brewer <brendon.brewer@gmail.com>
2015-07-11 12:58:06 +1200

    No need to normalised (some questions can have more effect than others)

--- Question.py
+++ Question.py
@@ -3,15 +3,20 @@ import numpy as np
 class Question:
 	"""
 	An object of this class is a question. Questions have text and a
-	direction. The text is the question itself, and the direction specifies
-	where an "agree" answer drags the user.
+	vector associated with them. The norm of the vector should be of order 1.
 	"""
 
-	def __init__(self, text, direction):
+	def __init__(self, text, vec):
 		"""
 		text = question text
-		direction = direction (gets normalised)
+		vec = direction and magnitude of the movement implied by strong
+				agreement with the statement
 		"""
 		self.text = text
-		self.direction = direction/np.linalg.norm(direction)
+		self.vec = vec
+		norm = np.linalg.norm(vec)
+		if norm > 2:
+			print("# Warning: created a strongly influential question.")
+		elif norm < 0.5:
+			print("# Warning: created a weakly influential question.")
 
